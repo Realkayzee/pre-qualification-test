40af5c6f1a4193d616b6569ca21bd26292786f29
kayzee <olaniyikazeem112@gmail.com>
2022-05-21 16:21:58 +0100

    pre qualification test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [inputs, setInputs] = useState({});
+  const [save, setSave] = useState([])
 
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+  // when the handlechange is console.log, our inputs
+  // will be of the form {ridername: " ", tier: "10000"}
+  // in our console
+
+
+  const handleSubmit = (event) => {
+    if (inputs) {
+      setSave(values => [...values, { inputs }])
+    }
+
+    console.log(save)
+    event.preventDefault();
+  }
+
+  // our save is the array storing up the object's data
+  // save will be of the format
+  // save = [
+  //  {ridername: "", tier: "10000"},
+  //  {ridername: "", tier: "10000"},
+  //  {ridername: "", tier: "10000"},
+  //  {ridername: "", tier: "10000"},
+  //  ]
+  // depending on the inputted data from users
+  const totalMoneySaved = () => {
+    for (let i = 0; i < save.length; i++) {
+      const [moneysave, setMoneysave] = useState([])
+      setMoneysave(data => [...data, parseInt(save[i].tier)])
+      moneysave.reduce((prev, cur) => prev + cur)
+    }
+    // the save[i].tier targetted the tier's values
+    // the parseInt convert its string to number
+    // moneysave is the array of the tires total number
+    // which can be calculated by using the javascript reduce method
+  }
+
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        // input for rider's name
+        <label>Rider's name:
+          <input
+            type="text"
+            name="ridername"
+            value={inputs.ridername || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <p>Select your tier:</p>
+        <select name="tier" value={inputs.tier || "10000"} onChange={handleChange}>
+          <option value="10000">Save 10,000 naira for 10%:{0.07 * 10000} cashback in a week </option>
+          <option value="20000">Save 20,000 naira for 12%:{0.12 * 20000} cashback in a week</option>
+          <option value="30000">Save 30,000 naira for 25%:{0.25 * 30000} cashback in a week</option>
+        </select>
+        <input type="submit" />
+      </form>
+
+      <h2 className='Data page'>Data Page of the Saving App</h2>
+      <p>The total money saved by all riders is {totalMoneySaved}</p>
+      <p>The breakdown per member is {save} </p>
+
+        // save consist of the numbers of rider that saved with the amount they save.
+    </div >
+  )
+}
 export default App;
